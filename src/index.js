9fca31fc88040884166a1a597272f481078b2490
Smanway Gupta <smanway.gupta@netsmartz.net>
2024-10-07 13:39:50 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 dotenv.config({
-    path: "./env"
-})
+  path: "./env",
+});
 
 connectDB()
-.then(()=>{
-    app.on((err)=>{
-        console.log("Error in connecting express:",err)
-    })
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`server is running at port: ${process.env.PORT}`)
-    })
-})
-.catch((err)=>{
-    console.log("Mongo DB Error:", err)
-})
-
+  .then(() => {
+    app.on((err) => {
+      console.log("Error in connecting express:", err);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB Error:", err);
+  });
 
 // Another Approach //
 /*
